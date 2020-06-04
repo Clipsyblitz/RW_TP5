ed91ccd1f88aff66c1c991fe0625537d3340ac1c
Malone JULIENNE <malone.julienne@etu.univ-grenoble-alpes.fr>
2020-06-04 14:10:36 +0200

    [RX][TP5] Echanges serveur terminés

--- ArchiveTPSocket/client.c
+++ ArchiveTPSocket/client.c
@@ -8,22 +8,20 @@
 /*									      */
 /*		Auteurs : ... 					*/
 /*									      */
-/******************************************************************************/	
-
+/******************************************************************************/
 
 #include <stdio.h>
-#include <curses.h> 		/* Primitives de gestion d'ecran */
+#include <curses.h> /* Primitives de gestion d'ecran */
 #include <sys/signal.h>
 #include <sys/wait.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-#include "fon.h"   		/* primitives de la boite a outils */
+#include "fon.h" /* primitives de la boite a outils */
 
 #define SERVICE_DEFAUT "1111"
-#define SERVEUR_DEFAUT "127.0.0.1"
+#define SERVEUR_DEFAUT "0.0.0.0"
 
-void client_appli (char *serveur, char *service);
-
+void client_appli(char *serveur, char *service);
 
 /*****************************************************************************/
 /*--------------- programme client -----------------------*/
@@ -31,48 +29,62 @@ void client_appli (char *serveur, char *service);
 int main(int argc, char *argv[])
 {
 
-	char *serveur= SERVEUR_DEFAUT; /* serveur par defaut */
-	char *service= SERVICE_DEFAUT; /* numero de service par defaut (no de port) */
-
+	char *serveur = SERVEUR_DEFAUT; /* serveur par defaut */
+	char *service = SERVICE_DEFAUT; /* numero de service par defaut (no de port) */
 
 	/* Permet de passer un nombre de parametre variable a l'executable */
-	switch(argc)
+	switch (argc)
 	{
- 	case 1 :		/* arguments par defaut */
-		  printf("serveur par defaut: %s\n",serveur);
-		  printf("service par defaut: %s\n",service);
-		  break;
-  	case 2 :		/* serveur renseigne  */
-		  serveur=argv[1];
-		  printf("service par defaut: %s\n",service);
-		  break;
-  	case 3 :		/* serveur, service renseignes */
-		  serveur=argv[1];
-		  service=argv[2];
-		  break;
-    default:
-		  printf("Usage:client serveur(nom ou @IP)  service (nom ou port) \n");
-		  exit(1);
+	case 1: /* arguments par defaut */
+		printf("serveur par defaut: %s\n", serveur);
+		printf("service par defaut: %s\n", service);
+		break;
+	case 2: /* serveur renseigne  */
+		serveur = argv[1];
+		printf("service par defaut: %s\n", service);
+		break;
+	case 3: /* serveur, service renseignes */
+		serveur = argv[1];
+		service = argv[2];
+		break;
+	default:
+		printf("Usage:client serveur(nom ou @IP)  service (nom ou port) \n");
+		exit(1);
 	}
 
 	/* serveur est le nom (ou l'adresse IP) auquel le client va acceder */
 	/* service le numero de port sur le serveur correspondant au  */
 	/* service desire par le client */
-	
-	client_appli(serveur,service);
+
+	client_appli(serveur, service);
 }
 
 /*****************************************************************************/
-void client_appli (char *serveur,char *service)
+void client_appli(char *serveur, char *service)
 
 /* procedure correspondant au traitement du client de votre application */
-
 {
-  
+	struct sockaddr_in *c;
+	struct sockaddr_in *s;
 
-/* a completer .....  */
+	int id_active = h_socket(AF_INET, SOCK_STREAM);
 
- }
+	adr_socket(0, "192.168.1.13", SOCK_STREAM, &c);
+
+	h_bind(id_active, c);
+
+	adr_socket(service, serveur, SOCK_STREAM, &s);
+
+	h_connect(id_active, s);
+
+	char *b_read = (char *)malloc(sizeof(char) * 100);
+	char *b_write = (char *)malloc(sizeof(char) * 100);
+
+	h_writes(id_active, "salut", 5);
+	h_reads(id_active, b_read, 5);
+	printf("Le message est : %s\n", b_read);
+
+	h_close(id_active);
+}
 
 /*****************************************************************************/
-
--- ArchiveTPSocket/serveur.c
+++ ArchiveTPSocket/serveur.c
@@ -9,63 +9,76 @@
 /*		Auteurs :  ....						      */
 /*		Date :  ....						      */
 /*									      */
-/******************************************************************************/	
+/******************************************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
 #include <curses.h>
 
-#include<sys/signal.h>
-#include<sys/wait.h>
-#include<stdlib.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
-#include "fon.h"     		/* Primitives de la boite a outils */
+#include "fon.h" /* Primitives de la boite a outils */
 
 #define SERVICE_DEFAUT "1111"
 
-void serveur_appli (char *service);   /* programme serveur */
+void serveur_appli(char *service); /* programme serveur */
 
-
-/******************************************************************************/	
+/******************************************************************************/
 /*---------------- programme serveur ------------------------------*/
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 
-	char *service= SERVICE_DEFAUT; /* numero de service par defaut */
-
+	char *service = SERVICE_DEFAUT; /* numero de service par defaut */
 
 	/* Permet de passer un nombre de parametre variable a l'executable */
 	switch (argc)
- 	{
-   	case 1:
-		  printf("defaut service = %s\n", service);
-		  		  break;
- 	case 2:
-		  service=argv[1];
-            break;
+	{
+	case 1:
+		printf("defaut service = %s\n", service);
+		break;
+	case 2:
+		service = argv[1];
+		break;
 
-   	default :
-		  printf("Usage:serveur service (nom ou port) \n");
-		  exit(1);
- 	}
+	default:
+		printf("Usage:serveur service (nom ou port) \n");
+		exit(1);
+	}
 
 	/* service est le service (ou numero de port) auquel sera affecte
 	ce serveur*/
-	
+
 	serveur_appli(service);
 }
 
-
-/******************************************************************************/	
+/******************************************************************************/
 void serveur_appli(char *service)
 
 /* Procedure correspondant au traitemnt du serveur de votre application */
 
 {
+	struct sockaddr_in *s;
 
-/* A completer ... */
+	int id_passive = h_socket(AF_INET, SOCK_STREAM);
 
+	adr_socket(service, NULL, SOCK_STREAM, &s);
+
+	h_bind(id_passive, s);
+
+	h_listen(id_passive, 5);
+
+	int id_active = h_accept(id_passive, s);
+
+	char *buffer = (char *)malloc(sizeof(char) * 100);
+
+	h_reads(id_active, buffer, 5);
+	printf("Le message est : %s\n", buffer);
+	h_writes(id_active, buffer, 5);
+
+	h_close(id_passive);
+	h_close(id_active);
 }
 
-/******************************************************************************/	
-
+/******************************************************************************/
