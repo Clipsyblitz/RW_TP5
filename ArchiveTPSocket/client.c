468cb85415140aef298b280181d65c7e9ad11438
Malone JULIENNE <malone.julienne@etu.univ-grenoble-alpes.fr>
2020-06-22 18:02:44 +0200

    [RX][TP5] Jeu terminé

--- ArchiveTPSocket/client.c
+++ ArchiveTPSocket/client.c
@@ -59,11 +59,73 @@ int main(int argc, char *argv[])
 	client_appli(serveur, service);
 }
 
+/* fonction permettant de vérifier que les couleurs insérer sont biens des couleurs du jeu.*/
+int verif_color(char *color, char res)
+{
+	int color_verif = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		if (res == color[i])
+			color_verif = 1;
+	}
+	if (color_verif == 0)
+		return 0;
+	return 1;
+}
+
+/* fonction permettant de vérifier que le message écris est de la bonne taille*/
+int verif_buffer(char *buffer, char *res, char *color)
+{
+	char c = *buffer;
+	int ct = 0;
+	int i = 0;
+	while (c != '\0')
+	{
+		if (i == 5)
+		{
+			return 1;
+		}
+		if (verif_color(color, c))
+		{
+			res[i] = c;
+			i++;
+		}
+		ct++;
+		c = *(buffer + ct);
+	}
+	if (i < 5)
+	{
+		printf("\n /!\\Erreurs message trop petit\n");
+		return 0;
+	}
+	return 1;
+}
+
+/* fonction appelée pour vérifier la conformité du message à envoyé.*/
+int verif_entry(char *buffer, char *color, char *res)
+{
+	return verif_buffer(buffer, res, color);
+}
+
+void print_color(char *color)
+{
+	printf("Les couleurs utilisées dans cette partie sont les suivantes : \n");
+	for (int i = 0; i < 5; i++)
+		printf(" %c,", color[i]);
+	printf("\n");
+}
+
 /*****************************************************************************/
 void client_appli(char *serveur, char *service)
 
 /* procedure correspondant au traitement du client de votre application */
 {
+	// Tableau des couleurs disponibles dans la partie
+
+	char *color = (char *)malloc(sizeof(char) * 5);
+
+	// Initialisation des sockets
+
 	struct sockaddr_in *c;
 	struct sockaddr_in *s;
 
@@ -80,9 +142,56 @@ void client_appli(char *serveur, char *service)
 	char *b_read = (char *)malloc(sizeof(char) * 100);
 	char *b_write = (char *)malloc(sizeof(char) * 100);
 
-	h_writes(id_active, "salut", 5);
-	h_reads(id_active, b_read, 5);
-	printf("Le message est : %s\n", b_read);
+	// Récupération des couleurs disponibles
+
+	h_reads(id_active, color, 5);
+
+	// Affichage des couleurs disponibles
+
+	print_color(color);
+
+	char *buffer_emission = (char *)malloc(sizeof(char) * 5);
+	char *answer = (char *)malloc(sizeof(char) * 2);
+	char *lives_buffer = (char *)malloc(sizeof(char) * 2);
+
+	int lives;
+
+	h_reads(id_active, lives_buffer, 2);
+
+	lives = atoi(lives_buffer);
+
+	printf("Nombre d'essais pour cette partie : %d \n", lives);
+
+	int value = -1;
+
+	while (lives != 0 && value != 5)
+	{
+		printf("Nombre d'essais restant : %d \n", lives);
+
+		do
+		{
+			fgets(b_write, 15, stdin);
+		} while (!verif_entry(b_write, color, buffer_emission));
+
+		h_writes(id_active, buffer_emission, 5);
+
+		h_reads(id_active, answer, 2);
+
+		value = answer[1];
+
+		if (value != 5)
+		{
+			lives--;
+			printf("Tu as %d bonne(s) couleur(s) et %d couleur(s) à la bonne position ! \n", answer[0], answer[1]);
+		}
+	}
+
+	if (value == 5)
+		printf("Bravo ! Tu as gagné ! \n");
+	else
+		printf("T'as perdu, dommage ! \n");
+
+	h_writes(id_active, "!!!!!", 5);
 
 	h_close(id_active);
 }
--- ArchiveTPSocket/serveur.c
+++ ArchiveTPSocket/serveur.c
@@ -17,6 +17,8 @@
 #include <sys/signal.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 #include "fon.h" /* Primitives de la boite a outils */
 
@@ -53,12 +55,59 @@ int main(int argc, char *argv[])
 	serveur_appli(service);
 }
 
+void interprete_answer(char *code, char *receive, char *answer)
+{
+	if (receive[0] != '!')
+	{
+
+		int ok_couleur = 0, ok_place = 0, couleur_presente = 0;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				if (i == j && code[i] == receive[i])
+				{
+					ok_place++;
+					ok_couleur++;
+					couleur_presente = 1;
+				}
+				if (couleur_presente != 1 && receive[i] == code[j])
+				{
+					couleur_presente = 1;
+					ok_couleur++;
+				}
+			}
+			couleur_presente = 0;
+		}
+
+		answer[0] = ok_couleur;
+		answer[1] = ok_place;
+	}
+	else
+		answer[0] = '@';
+}
+
+void init_code(char *code, char *color)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		code[i] = color[rand() % 5];
+	}
+}
 /******************************************************************************/
 void serveur_appli(char *service)
 
 /* Procedure correspondant au traitemnt du serveur de votre application */
 
 {
+	// Tableau des couleurs disponibles dans la partie
+
+	srand(time(NULL));
+
+	char color[5] = {'r', 'v', 'b', 'j', 'n'};
+
+	// Initialisation des sockets
+
 	struct sockaddr_in *s;
 
 	int id_passive = h_socket(AF_INET, SOCK_STREAM);
@@ -71,11 +120,35 @@ void serveur_appli(char *service)
 
 	int id_active = h_accept(id_passive, s);
 
-	char *buffer = (char *)malloc(sizeof(char) * 100);
+	// Envoie des couleurs disponibles au client
 
-	h_reads(id_active, buffer, 5);
-	printf("Le message est : %s\n", buffer);
-	h_writes(id_active, buffer, 5);
+	h_writes(id_active, color, 5);
+
+	// Envoie du nombre de vies au client
+
+	h_writes(id_active, "10", 2);
+
+	// Initialisation du code
+
+	char code[5];
+	init_code(code, color);
+
+	// Récupération des message et interpretation
+
+	char receive[5];
+	char answer[2];
+
+	while (!(strcmp(receive, "!!!!!") == 0))
+	{
+		h_reads(id_active, receive, 5);
+
+		interprete_answer(code, receive, answer);
+
+		if (answer[0] != '@')
+		{
+			h_writes(id_active, answer, 2);
+		}
+	}
 
 	h_close(id_passive);
 	h_close(id_active);
